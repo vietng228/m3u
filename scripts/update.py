a47cc83a87d056e545a6db91ada70a661cd1bec7
Việt Xiaomi <126229808+vietng228@users.noreply.github.com>
2026-08-15 21:27:46 +0700

    Update update.py

--- scripts/update.py
+++ scripts/update.py
@@ -6,8 +6,11 @@ import requests
 import unicodedata
 
 SOURCE_URL = "http://tv.vietanhtv.top/tv"
-TARGET_FILE = "m3u.m3u"
-
+TARGET_FILES = [
+    "m3u.m3u",
+    "new.m3u",
+    "test.m3u",
+]
 
 def fetch(url: str) -> str:
     r = requests.get(
